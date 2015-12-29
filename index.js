1bb5ff940f2bf5b014bd5bdc34bfca63f1a7cd84
zoubin <zoubin04@gmail.com>
2015-12-29 18:29:16 +0800

    Fix sugar constructor Reduce

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Reduce = require('./lib/reduce')
 exports = module.exports = Reduce()
-exports.Reduce = Reduce
+exports.Reduce = Reduce.bind(null)
 exports.watch = require('./lib/watch')
 exports.lazypipe = require('lazypipe')
 exports.dest = require('vinyl-fs').dest
