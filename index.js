01d7b585cdd280e087eb0f7cf6fa7cfbef4562fc
zoubin <zoubin04@gmail.com>
2015-12-31 16:04:34 +0800

    Allow null entries

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Reduce = require('./lib/reduce')
-exports = module.exports = Reduce()
-exports.Reduce = Reduce.bind(null)
+exports = module.exports = new Reduce()
+exports.Reduce = function () {
+  return new Reduce()
+}
 exports.watch = require('./lib/watch')
 exports.lazypipe = require('lazypipe')
 exports.dest = require('vinyl-fs').dest
